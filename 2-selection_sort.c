e4253ec8b271dbadeb1c07decfaafc7f0110512e
steven <12992@holbertonstudents.com>
2026-04-10 16:24:04 +0200

    add 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,35 +20,35 @@
 
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_idx;
-int tmp;
+	size_t i, j, min_idx;
+	int tmp;
 
-if (array == NULL || size < 2)
-	return;
+	/* 1. Accolades ajoutées pour éviter l'erreur d'indentation */
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
 
 	for (j = 0; j < size - 1; j++)
 	{
-
-		int min_idx = j;
-
-		for (i = j + 1; i < size - 1; i++)
+		min_idx = j;
+		for (i = j + 1; i < size; i++)
 		{
-
 			if (array[i] < array[min_idx])
 			{
 				min_idx = i;
 			}
 		}
 
-
+		/* 2. Comparaison entre deux size_t (min_idx et j) */
 		if (min_idx != j)
 		{
+			/* 3. Utilisation de tmp pour le swap (règle l'erreur unused) */
 			tmp = array[j];
 			array[j] = array[min_idx];
+			array[min_idx] = tmp;
 
 			print_array(array, size);
 		}
-
-
 	}
 }
